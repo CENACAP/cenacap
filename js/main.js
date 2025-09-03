7d806f5262b6ed3e7a8d051cfc8c941d3dcdf6a5
Piter <pedromedro777@gmail.com>
2025-09-03 18:54:14 -0400

    e

--- js/main.js
+++ js/main.js
@@ -5,15 +5,14 @@ const navLinks = document.querySelector('.nav-links');
 if (menuToggle && navLinks) {
     menuToggle.addEventListener('click', () => {
         navLinks.classList.toggle('active');
-        // También puedes añadir una clase para cambiar el icono de la hamburguesa
-        menuToggle.classList.toggle('active'); 
+        menuToggle.classList.toggle('active');
     });
 }
 
 // Funcionalidad para la barra de navegación que cambia de color al hacer scroll
 window.addEventListener('scroll', () => {
     const nav = document.querySelector('.main-nav');
-    if (nav) { // Asegurarse de que nav existe
+    if (nav) {
         if (window.scrollY > 50) {
             nav.classList.add('scrolled');
         } else {
